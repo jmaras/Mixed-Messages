20f7772e441536c7a38e4b067e7f3c426640610b
jmaras <julianmaras@gmail.com>
2021-03-24 19:33:16 +0100

    added object

--- WITH-LOVE.js
+++ WITH-LOVE.js
@@ -1,23 +1,24 @@
-let salution = ["darling", "sweetie pie", "babe", "honey", "sweetheart"];
-let activity = [
+const messageGenerator = {
+  salution : ["darling", "sweetie pie", "babe", "honey", "sweetheart"],
+  activity : [
   "make babies",
   "eat ice cream",
   "take a walk through the park",
   "have dinner",
   "netflix and chill",
-];
-let worship = [
+  ],
+  worship : [
   "I love you so much!",
   "Yours forever,",
   "I wanna touch your butt badly.",
   "Let's fly to the moon!",
-  "Ouuuweee!!!",
-];
-
+  "Ouuuweee!!!"
+  ]
+}
 console.log(
   `Welcome to "WITH-LOVE", the love message creator. Your Love Message is: "Hey ${
-    salution[Math.floor(Math.random() * 5)]
-  }, would you like to ${activity[Math.floor(Math.random() * 5)]} together? ${
-    worship[Math.floor(Math.random() * 5)]
-  } Yours sincerly, Secret Crush Bot.`
+    messageGenerator.salution[Math.floor(Math.random() * messageGenerator.salution.length)]
+  }, would you like to ${messageGenerator.activity[Math.floor(Math.random() * messageGenerator.activity.length)]} together? ${
+    messageGenerator.worship[Math.floor(Math.random() * messageGenerator.worship.length)]
+  } Yours sincerly, WITH-LOVE Bot.`
 );
